fe5d600a1470b8381c576e2bad873ade1c569dda
Vladyslav Bagatskyy <121740176+VladBagat@users.noreply.github.com>
2024-08-17 13:53:22 +0100

    Add files via upload

---
+++ DiscordBot/main.py
@@ -0,0 +1,19 @@
+import discord
+from api_key import discord_key
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+client.run(discord_key)
