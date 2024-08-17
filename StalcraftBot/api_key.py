35eb37796eeadbe199ff365946c090bf4bbc9507
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-08-17 21:00:11 +0100

    Finishing the cleanup

---
+++ StalcraftBot/api_key.py
@@ -0,0 +1,7 @@
+user_id = "482"
+secret = "GLhlogbsFfqei0IzCMvvsCJGwmvCiBliyATwbhKp"
+
+database_key = 'FtxsjEa28w7zbPr'
+
+discord_key = "MTIwNzQ2MTM4MjE0OTM3ODA1OQ.G0xwvD.yW2n8KPM911q01PAl6eXxeWxKoYVTk9RQOkIYg"
+
