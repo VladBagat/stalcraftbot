4ebbd6a4be9c8e8052adbef4272a4801f2468527
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-08-20 17:54:14 +0100

    Updated gitignore

--- Methods/API_requests.py
+++ Methods/API_requests.py
@@ -1,5 +1,5 @@
 import requests
-from api_key import client_id, secret, client_token
+from api_key import client_token, user_token
 
 def retrieve_login(user):
 
@@ -8,10 +8,11 @@ def retrieve_login(user):
 
     # Set up the headers with the OAuth token
     headers = {
+        "Content-Type": "application/json",
         'Authorization': f'Bearer {client_token}'
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.request("GET", url, headers=headers)
 
     if response.status_code == 200:
     
@@ -27,23 +28,25 @@ def retrieve_login(user):
 
 def retrieve_clan_members():
 
+    #Og Groove Street id
     clan_id = "9d49d17e-8b20-45de-b0d1-6574bcd92a3d"
+    
 
     #API endpoint
-    url = f'https://eapi.stalcraft.net/EU/clan/{clan_id}/info'
+    url = f'https://eapi.stalcraft.net/EU/clan/{clan_id}/members'
 
     # Set up the headers with the OAuth token
     headers = {
-        'Authorization': f'Bearer {client_token}'
+        "Content-Type": "application/json",
+        'Authorization': f'Bearer {user_token}'
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.request("GET", url, headers=headers)
 
     if response.status_code == 200:
     
         print(response.json())
         
     else:
-        return 
-    
-retrieve_clan_members()
+        
+        print(f"Error: {response.status_code}, {response.text}")
