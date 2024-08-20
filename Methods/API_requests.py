12c7a6e46f5e8de72f816a02b7bb30435ff9f787
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-08-20 12:41:57 +0100

    Minor changes

--- Methods/API_requests.py
+++ Methods/API_requests.py
@@ -1,5 +1,5 @@
 import requests
-from api_key import user_id, secret, client_token
+from api_key import client_id, secret, client_token
 
 def retrieve_login(user):
 
@@ -23,3 +23,27 @@ def retrieve_login(user):
         error = "Игрок не найден"
 
         return error
+    
+
+def retrieve_clan_members():
+
+    clan_id = "9d49d17e-8b20-45de-b0d1-6574bcd92a3d"
+
+    #API endpoint
+    url = f'https://eapi.stalcraft.net/EU/clan/{clan_id}/info'
+
+    # Set up the headers with the OAuth token
+    headers = {
+        'Authorization': f'Bearer {client_token}'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+    
+        print(response.json())
+        
+    else:
+        return 
+    
+retrieve_clan_members()
