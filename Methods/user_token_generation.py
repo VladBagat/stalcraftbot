54bdf6750a80fb934fd2d413a1b43100c61407bd
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-08-20 12:42:10 +0100

    Added manual token generation

--- Methods/user_token_generation.py
+++ Methods/user_token_generation.py
@@ -1,19 +1,24 @@
 import requests
-from Methods.api_key import client_id, secret, client_token
+from api_key import client_id, secret, client_token, user_code
 
 def get_token():
     url = "https://exbo.net/oauth/token"
     data = {
         "client_id": f"{client_id}",  
         "client_secret": f"{secret}",  
-        "grant_type": "client_credentials",
-        "scope": "" 
+        "code": f"{user_code}",
+        "grant_type": "authorization_code",
+        "redirect_uri": "http://ssdimages.rf.gd"
+
     }
 
     response = requests.post(url, data=data)
 
     if response.status_code == 200:
-        token = response.json().get("access_token")
+        token = response.json()
+        print(token)
         return token
     else:
         print(f"Error: {response.status_code}, {response.text}")
+
+get_token()
