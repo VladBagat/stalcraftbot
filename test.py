42849e1ab77e2e8dfef7cbc50c3d050feaf0cbf1
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-08-26 20:14:49 +0100

    Updated gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-from Methods.API_requests import retrieve_clan_members
-from Methods.manual.user_token_generation import get_token
-from Methods.functions import parse_nickname
-
-
-dictionary = {'sigma':(10, 20, 30)}
-
-print(list(dictionary.values()))
