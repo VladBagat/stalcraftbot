35f675a24e5862d0f4f7ffd4699d1aa499fbb388
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-09-10 22:41:27 +0100

    Added testing compatibility

--- cogs/test.py
+++ cogs/test.py
@@ -60,7 +60,7 @@ class Test(commands.Cog):
 
         late_players = []
 
-        for i in range(0, 30):
+        for i in range(len(players)):
             if not was_on_cw[i] and not on_hiatus[i]:
                 late_players.append(players[i])
 
