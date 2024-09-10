42211dc0b40d5ef27584d541852be7b1fb078f08
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-09-10 22:41:27 +0100

    Added testing compatibility

--- Methods/database/database_requests.py
+++ Methods/database/database_requests.py
@@ -1,7 +1,12 @@
 from psycopg2.pool import SimpleConnectionPool
 from Methods.API_requests import retrieve_clan_members
 from keys import keys
+import os
 
+if os.getenv('heroku'):
+    database_name = 'players'
+else:
+    database_name = 'players_test'
 
 def connect_to_database() -> SimpleConnectionPool:
     pool = SimpleConnectionPool(minconn=1, maxconn=10, dbname=keys.dbname, user=keys.user, password=keys.password, host=keys.host)
@@ -10,7 +15,7 @@ def connect_to_database() -> SimpleConnectionPool:
 #Functions for Scheduled.HiatusButton
 def fetch_hiatus(conn, user_nickname):
     with conn.cursor() as cur:
-        fetch_query = 'SELECT hiatus_num, is_hiatus FROM players WHERE name = %s'
+        fetch_query = f'SELECT hiatus_num, is_hiatus FROM {database_name} WHERE name = %s'
         cur.execute(fetch_query, (user_nickname,))
         results = cur.fetchone()
 
@@ -19,7 +24,7 @@ def fetch_hiatus(conn, user_nickname):
 def update_hiatus(conn, user_data_list : list) -> None:
     print(user_data_list)
     with conn.cursor() as cur:
-        update_query = 'UPDATE players SET hiatus_num = %s, is_hiatus = %s WHERE name = %s'
+        update_query = f'UPDATE {database_name} SET hiatus_num = %s, is_hiatus = %s WHERE name = %s'
         cur.executemany(update_query, user_data_list)
         conn.commit()
 
@@ -27,10 +32,10 @@ def update_hiatus(conn, user_data_list : list) -> None:
 def daily_online_hiatus(conn):
     #Returns all users with is_hiatus set True and sets is_hiatus to False
     with conn.cursor() as cur:
-        fetch_query = 'SELECT name, is_hiatus FROM players'
+        fetch_query = f'SELECT name, is_hiatus FROM {database_name}'
         cur.execute(fetch_query)
         results = cur.fetchall()
-        update_query = 'UPDATE players SET is_hiatus = 0'
+        update_query = f'UPDATE {database_name} SET is_hiatus = 0'
         cur.execute(update_query)
         conn.commit()
 
@@ -39,6 +44,6 @@ def daily_online_hiatus(conn):
 def increment_player_penalty(conn, players:list):
     with conn.cursor() as cur:
         data = [(100000, player) for player in players]
-        update_penalty_query = "UPDATE players SET penalty = penalty + %s WHERE name = %s AND NOT rank = 'RECRUIT'"
+        update_penalty_query = f"UPDATE {database_name} SET penalty = penalty + %s WHERE name = %s AND NOT rank = 'RECRUIT'"
         cur.executemany(update_penalty_query, data)
         conn.commit()
