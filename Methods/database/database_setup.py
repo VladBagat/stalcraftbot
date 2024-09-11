5ab4c0bd1900c8dbbd66ec173899d38f90418ee2
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-09-11 20:28:48 +0100

    Implemented auto-sync of clan members. Bug fixes

--- Methods/database/database_setup.py
+++ Methods/database/database_setup.py
@@ -1,5 +1,5 @@
 import psycopg2 as psycopg
-from Methods.API_requests import retrieve_clan_members
+from Methods.API_requests import parse_clan_members
 from keys import keys
 
 
@@ -7,11 +7,8 @@ from keys import keys
 def create_players_table(conn):
     cur = conn.cursor()
     table_arguments = '''(
-        id serial PRIMARY KEY,
         name text,
         rank text,
-        brawl_skip integer,
-        tournament_skip integer,
         hiatus_num integer,
         is_hiatus integer, 
         penalty integer
@@ -28,8 +25,9 @@ def create_players_table(conn):
     return result
 
 def prepare_players_data():
-    names, ranks = prepare_players()
-    data = [(name, rank, 0, 0, 2, 0, 0) for name, rank in zip(names, ranks)]
+
+    names, ranks = parse_clan_members()
+    data = [(name, rank, 2, 0, 0) for name, rank in zip(names, ranks)]
     return data
 
 def initiate_database():
@@ -45,30 +43,17 @@ def connect_to_database():
     conn = psycopg.connect(dbname=keys.dbname, user=keys.user, password=keys.password, host=keys.host)
     return conn
 
-def prepare_players():
-    names = []
-    ranks = []
-
-    players = retrieve_clan_members()
-
-    for i in range(len(players)):
-        names.append(players[i].get("name"))
-        ranks.append(players[i].get("rank"))
-
-    return names, ranks
-
 def refresh_players():
     pass
 
 def insert_players(conn, data):
     cur = conn.cursor()
-    cur.executemany("INSERT INTO players (name, rank, brawl_skip, tournament_skip, hiatus_num, is_hiatus, penalty) VALUES (%s, %s, %s, %s, %s, %s, %s)", data)
+    cur.executemany("INSERT INTO players (name, rank, hiatus_num, is_hiatus, penalty) VALUES (%s, %s, %s, %s, %s)", data)
     conn.commit()
     cur.close()
 
 def clone_database(conn):
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS players_test AS TABLE players WITH DATA")
-    #cur.execute("INSERT INTO nplayers_test SELECT * FROM players")
     conn.commit()
     cur.close()
